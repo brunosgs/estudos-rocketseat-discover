d872795ca4e560b3356933ee9436d77eef295c40
Bruno Gomes <brunosoges@gmail.com>
2022-08-11 08:09:11 -0300

    Utilizando expressões e operadores part 2

---
+++ Trilha Fundamentar/javascript/expressoesOperadores/groupingOperator.js
@@ -0,0 +1,12 @@
+/*
+    Expressões e operadores
+
+    - Grouping operator
+*/
+
+let total = 2 + 3 * 5 // Existe a ideia de precedência na matemática
+console.log(total)
+
+// Fazendo a troca da precedência com ( )
+let newTotal = (2 + 3) * 5
+console.log(newTotal)
---
+++ Trilha Fundamentar/javascript/expressoesOperadores/operadoresAritmeticos.js
@@ -0,0 +1,35 @@
+/*
+    Expressões e operadores
+
+    - Operadore aritméticos
+*/
+
+// Multiplicação *
+console.log(3.5 * 5)
+
+// Divisão /
+console.log(15 / 5)
+
+// Soma +
+console.log(36 + 69)
+
+// Subtração -
+console.log(36 - 69)
+
+// Resto da divisão %
+let remainder = 11 % 3
+console.log(remainder)
+
+// Incremento ++
+let increment = 0
+increment++ // OBS: ele será incrementando após, só quando ele finalizar essa linha
+++increment // OBS: ele será incrementando antes de finalizar a linha
+console.log(increment)
+
+// Decremento --
+let decrement = 0
+decrement-- // Segue a mesma regra do incremento, só vai decrementar após, quando finalizar a linha
+console.log(decrement)
+
+// Exponencial **
+console.log(3 ** 3)
