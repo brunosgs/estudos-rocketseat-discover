ae7f92e9b0e50c64424a72f0efbf2bdb35f0c5fd
Bruno Gomes <brunosoges@gmail.com>
2022-08-16 13:04:19 -0300

    Utilizando expressões e operadores part 4

---
+++ Trilha Fundamentar/javascript/expressoesOperadores/operadoresAtribuicao.js
@@ -0,0 +1,39 @@
+/*
+    Expressões e operadores
+
+    - Operadores de atribuição (Assignment)
+*/
+
+// assignment normal:
+let assigNormal = 1
+console.log(assigNormal)
+
+// addition assignment (adição):
+let assigAdicao = 1
+assigAdicao += 2
+console.log(assigAdicao)
+
+// subtraction assignment (subtração):
+let assigSubtracao = 1
+assigSubtracao -= 1
+console.log(assigSubtracao)
+
+// multiplication assignment (multiplacação):
+let assigMultiplicacao = 1
+assigMultiplicacao *= 2
+console.log(assigMultiplicacao)
+
+// division assignment (divisão):
+let assigDivisao = 1
+assigDivisao /= 2
+console.log(assigDivisao)
+
+// exponetiation assignment (exponenciação):
+let assigExponenciacao = 3
+assigExponenciacao **= 2
+console.log(assigExponenciacao)
+
+// remainder assignment (resto de divisão):
+let assigRestoDivisao = 1
+assigRestoDivisao %= 2
+console.log(assigRestoDivisao)
---
+++ Trilha Fundamentar/javascript/expressoesOperadores/operadoresLogicos.js
@@ -0,0 +1,19 @@
+/*
+    Expressões e operadores
+
+    - Operadores lógicos (logical operators)
+        * São dois valores do tipo booleano que quando verificados vai resultar em verdadeiro ou falso.
+*/
+
+let pao = true
+let queijo = true
+
+// AND (&&)
+console.log(pao && queijo)
+
+// OR (||)
+console.log(pao || queijo)
+
+// NOT (!)
+console.log(!pao)
+console.log(!queijo)
